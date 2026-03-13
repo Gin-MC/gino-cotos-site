038de4ed2399a18342ed951c96f33e0905d2e034
Gin-MC <cotosgino@gmail.com>
2026-03-13 14:58:44 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+// 1. IMPORTAMOS EL ADAPTADOR DE VERCEL
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +9,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  // 2. USAMOS VERCEL COMO MOTOR
+  adapter: vercel(),
 });
